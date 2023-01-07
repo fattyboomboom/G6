e4498a78c5693b59ee718842c39e990ebfaba5b7
Dre <42796363+Dre-Mylove@users.noreply.github.com>
2023-01-07 11:59:38 -0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <v-app>
+    <!-- <v-app-bar color="black"></v-app-bar> -->
+    <Navbar></Navbar>
     <v-main>
-      <HelloWorld/>
+      <ProfileAvatarVue/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/NavBar.vue";
+import ProfileAvatarVue from "./components/ProfileAvatar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Navbar,
+    ProfileAvatarVue,
   },
 
   data: () => ({
     //
   }),
-}
+};
 </script>
