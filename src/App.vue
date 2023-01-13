89feb70a123342518babadd3176ff59c1bf5778e
Dre <42796363+Dre-Mylove@users.noreply.github.com>
2023-01-13 13:58:35 -0800

    Add files via upload
    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app>
+  <v-app class="app">
     <!-- <v-app-bar color="black"></v-app-bar> -->
     <Navbar></Navbar>
     <v-main>
-      <ProfileAvatarVue name="Tom Anderson" major="Computer Science" />
+      <!-- <ProfileAvatarVue name="Tom Anderson" major="Computer Science" />
       <WallPostVue />
       <MyClassesVue />
-      <DashboardFriendsVue />
+      <DashboardFriendsVue /> -->
     </v-main>
   </v-app>
 </template>
@@ -14,20 +14,26 @@
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 import Navbar from "./components/NavBar.vue";
-import ProfileAvatarVue from "./components/ProfileAvatar.vue";
-import MyClassesVue from "./components/MyClasses.vue";
-import DashboardFriendsVue from "./components/DashboardFriends.vue";
-import WallPostVue from "./components/WallPost.vue";
+// import ProfileAvatarVue from "./components/ProfileAvatar.vue";
+// import MyClassesVue from "./components/MyClasses.vue";
+// import DashboardFriendsVue from "./components/DashboardFriends.vue";
+// import WallPostVue from "./components/WallPost.vue";
 
 export default {
   name: "App",
 
   components: {
-    WallPostVue,
+    // WallPostVue,
     Navbar,
-    ProfileAvatarVue,
-    MyClassesVue,
-    DashboardFriendsVue,
+    // ProfileAvatarVue,
+    // MyClassesVue,
+    // DashboardFriendsVue,
   },
 };
 </script>
+
+<style>
+.app {
+  background-color: whitesmoke;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
+
 
 createApp(App)
   .use(vuetify)
