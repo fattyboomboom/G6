fa8a851fe4e157bdaede53b06b978343a24b4d19
Dre <42796363+Dre-Mylove@users.noreply.github.com>
2023-01-30 18:02:14 -0800

    updated routing

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,16 @@
 <template>
-  <v-app class="app">
-    <!-- <v-app-bar color="black"></v-app-bar> -->
-    <Navbar></Navbar>
-    <v-main>
-      <!-- <ProfileAvatarVue name="Tom Anderson" major="Computer Science" />
-      <WallPostVue />
-      <MyClassesVue />
-      <DashboardFriendsVue /> -->
-    </v-main>
-  </v-app>
+ 
+  <router-view />
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import Navbar from "./components/NavBar.vue";
-// import ProfileAvatarVue from "./components/ProfileAvatar.vue";
-// import MyClassesVue from "./components/MyClasses.vue";
-// import DashboardFriendsVue from "./components/DashboardFriends.vue";
-// import WallPostVue from "./components/WallPost.vue";
+// import NavBar from './components/NavBar.vue';
 
-export default {
-  name: "App",
-
-  components: {
-    // WallPostVue,
-    Navbar,
-    // ProfileAvatarVue,
-    // MyClassesVue,
-    // DashboardFriendsVue,
-  },
-};
+// export default {
+//   name: "AppView",
+//   components: {
+//     NavBar
+//   },
+// };
 </script>
 
-<style>
-.app {
-  background-color: whitesmoke;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { createRouter, createWebHistory } from 'vue-router'
 
 loadFonts()
 
+import BookResellView from './views/BookResellView.vue'
+import HomeView from './views/HomeView.vue'
+import AboutViewVue from './views/AboutView.vue'
+import WelcomeView from './views/WelcomeView.vue'
 
-createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/bookresell', component:BookResellView},
+    { path: '/home', component: HomeView},
+    { path: '/about', component: AboutViewVue},
+    { path: '/', component: WelcomeView}
+  ]
+
+});
+
+createApp(App).use(router)
   .use(vuetify)
   .mount('#app')
