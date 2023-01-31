33e85adf24f5d94aa6de0052e6cccf753de6c331
Dre <42796363+Dre-Mylove@users.noreply.github.com>
2023-01-30 17:58:46 -0800

    updated routing

---
+++ src/views/BookResellView.vue
@@ -0,0 +1,21 @@
+<template>
+  <BookListingsFilter />
+  <BookListings />
+  <NavBar /></template>
+
+<script>
+import BookListings from "@/components/Resell/BookListings.vue";
+import BookListingsFilter from "@/components/Resell/BookListingsFilter.vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "BookResellView",
+  components: {
+    BookListings,
+    BookListingsFilter,
+    NavBar
+  },
+};
+</script>
+
+
