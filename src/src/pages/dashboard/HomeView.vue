aedce1d18619e1a7db0b4156370cea137edbd070
aaron bartee <zeebra901@nevada.unr.edu>
2023-01-31 19:26:18 -0800

    compiled files from dre and lauren

---
+++ src/src/pages/dashboard/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <DashboardFriends />
+  <MyClasses />
+  <ProfileAvatar />
+  <WallPost />
+  <NavBar />
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import DashboardFriends from "./DashboardFriends.vue";
+import MyClasses from "./MyClasses.vue";
+import ProfileAvatar from "./ProfileAvatar.vue";
+import WallPost from "./WallPost.vue";
+import NavBar from "@/globalcomponents/NavBar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    DashboardFriends,
+    MyClasses,
+    ProfileAvatar,
+    WallPost,
+    NavBar,
+  },
+};
+</script>
+
+<style scoped>
+body {
+  background-color: #fdf0d5;
+}
+</style>
---
+++ src/src/pages/tester/testPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <filter-menu></filter-menu>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+filter-menu {
+  margin-left:150px
+}
+h1 {
+  margin-left: 150px
+}
+</style>
