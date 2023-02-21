c7b543919a993c9cc2f01fcb2bf4c7ccc9f98205
lfeldman1 <lfeldman@nevada.unr.edu>
2023-02-21 09:36:00 -0800

    Added Announcements to Main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import NoteCards from './pages/notes/NotesCards.vue'
 import BookResell from './pages/bookresell/BookListings.vue'
-// import FilterMenu from './globalcomponents/BookListingsFilter.vue'
+import FilterMenu from './globalcomponents/BookListingsFilter.vue'
 import TestPage from './pages/tester/testPage.vue'
 import DashBoard from './pages/dashboard/HomeView.vue'
 import DisabilityPage from './pages/DRC/HelloWorld.vue'
@@ -13,9 +15,7 @@ import LoginPage from './pages/login/WelcomeView.vue'
 import NavBar from './globalcomponents/NavBar.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import SuperUserDash from "./pages/superuser/SuperUserDash.vue"
-import ModEdit from "./pages/moderator/ModEdit.vue"
-//import AnnouncementsDropdown from './globalcomponents/AnnouncementsDropdown.vue'
+import AnnouncementsDropdown from './globalcomponents/AnnouncementsDropdown.vue'
 
 loadFonts()
 
@@ -29,9 +29,7 @@ const router = createRouter({
     { path: '/DRC', component: DisabilityPage },
     { path: '/about', component:  AboutPage },
     { path: '/explore', component:  ExplorePage },
-    { path: '/', component: LoginPage },
-    { path: '/super', component: SuperUserDash},
-    { path: '/mod', component: ModEdit}
+    { path: '/', component: LoginPage }
   ]
 });
 
@@ -40,6 +38,7 @@ createApp(App)
   .use(vuetify)
   .use(router)
   .component('note-cards', NoteCards)
-  //.component('filter-menu', FilterMenu)
+  .component('filter-menu', FilterMenu)
   .component('nav-bar', NavBar)
+  .component ('announcements-dropdown', AnnouncementsDropdown)
   .mount('#app')
