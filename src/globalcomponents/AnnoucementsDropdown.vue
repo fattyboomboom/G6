0805df749ce841280ca7d04f9f93145c425ba7ae
lfeldman1 <lfeldman@nevada.unr.edu>
2023-02-21 09:26:24 -0800

    Announcements

--- src/globalcomponents/AnnoucementsDropdown.vue
+++ src/globalcomponents/AnnoucementsDropdown.vue
@@ -1,50 +1,86 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="300"
-  >
-    <v-list :items="items"></v-list>
-  </v-card>
+  <div class="announcements-dropdown">
+    <button class="announcements-dropdown-button" @click="isOpen = !isOpen">
+      Announcements <span class="arrow" :class="{ 'open': isOpen }"></span>
+    </button>
+    <ul class="announcements-dropdown-list" v-if="isOpen">
+      <li v-for="(announcement, index) in announcements" :key="index">
+        {{ announcement }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-    name: "AnnoucementsDropdown",
-  };
-    data: () => ({
-      items: [
-        { type: 'subheader', title: <center>'Announcements'</center> },
-        {
-          title: 'Quiz #1 - CS 365',
-          value: 1,
-        },
-        { type: 'divider' },
-        {
-          title: 'Test 1 Study Session - 2/12/23',
-          value: 2,
-        },
-        { type: 'divider' },
-        {
-          title: 'No class on 2/13/23',
-          value: 3,
-        },
-        { type: 'divider' },
-        {
-          title: 'Meeting on Zoom - 2/14/23',
-          value: 4,
-        },
-        { type: 'divider' },
-        {
-          title: 'Homework Review - 2/16/23',
-          value: 5,
-        },
-        { type: 'divider' },
-        {
-          title: 'Midterm Review Session - 2/21/23',
-          value: 6,
-        },
-      ],
-    }),
+import { ref } from 'vue';
+
+export default {
+  name: 'AnnouncementsDropdown',
+  setup() {
+    const isOpen = ref(false);
+    const announcements = ref([
+      'Announcement 1',
+      'Announcement 2',
+      'Announcement 3'
+    ]);
+
+    return {
+      isOpen,
+      announcements
+    };
+  }
+};
 </script>
 
-  
+<style>
+.announcements-dropdown {
+  position: relative;
+}
+
+.announcements-dropdown-button {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  cursor: pointer;
+  display: block;
+  font-size: 16px;
+  padding: 10px 15px;
+  text-align: left;
+  width: 100%;
+}
+
+.announcements-dropdown-button .arrow {
+  display: inline-block;
+  margin-left: 10px;
+  width: 0;
+  height: 0;
+  border: 5px solid transparent;
+  border-top-color: #333;
+  vertical-align: middle;
+  transition: transform 0.3s ease-in-out;
+}
+
+.announcements-dropdown-button .arrow.open {
+  transform: rotate(180deg);
+}
+
+.announcements-dropdown-list {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-top: none;
+  border-radius: 0 0 3px 3px;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  z-index: 1;
+}
+
+.announcements-dropdown-list li {
+  padding: 10px 15px;
+  text-align: left;
+}
+
+</style>
